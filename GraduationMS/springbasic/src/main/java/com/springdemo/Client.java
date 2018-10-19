78382fa6f6f3b387e4d444c324c1a47697a0b837
xuruiqian <ruiqian_xu@sina.com>
2018-10-19 12:49:18 +0800

    update

--- GraduationMS/springbasic/src/main/java/com/springdemo/Client.java
+++ GraduationMS/springbasic/src/main/java/com/springdemo/Client.java
@@ -1,10 +1,12 @@
 package com.springdemo;
 
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Client {
-    public static void main(String[] args) {
+    @Test
+    public void Test() {
         ApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"applicationContext.xml"});
         Api api = (ApiTwo)context.getBean("api");
         String result = api.test(123);
