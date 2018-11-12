dbc9306f368a60011d995127d3c0851481e0e48b
xuruiqian <ruiqian_xu@sina.com>
2018-11-12 21:24:59 +0800

    u

--- GraduationMS/mybatisbasicdemo/src/test/java/mybatistest/TestMyBatisQuery.java
+++ GraduationMS/mybatisbasicdemo/src/test/java/mybatistest/TestMyBatisQuery.java
@@ -14,9 +14,9 @@ public class TestMyBatisQuery extends TestMyBatisBase {
         QuickNavigationWithBLOBs result = quickNavigationMapper.selectByPrimaryKey("2ce64a8c-95e4-40e2-903f-579b73efe1f9");
         System.out.println(result.getText().toString());
 
-        List<QuickNavigationWithBLOBs> quickNavigations= quickNavigationMapper.selectAll();
-        for (QuickNavigationWithBLOBs item:
-        quickNavigations) {
+        List<QuickNavigationWithBLOBs> quickNavigations = quickNavigationMapper.selectAll();
+        for (QuickNavigationWithBLOBs item :
+                quickNavigations) {
             System.out.println(item.getText().toString());
         }
 
